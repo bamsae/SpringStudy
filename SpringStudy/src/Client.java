817f09c2e9ddfc0af2586d9b79dc76e724c9f242
Taegyun Woo <ted2231@gmail.com>
2013-04-22 18:05:11 +0900

    first0422

---
+++ SpringStudy/src/Client.java
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: skplanet
+ * Date: 13. 4. 22.
+ * Time: 오후 5:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class Client {
+
+    private Log log;
+
+    public void setLog(Log log) {
+        this.log = log;
+    }
+
+    public void biz() {
+        log.write("log");
+    }
+}
---
+++ SpringStudy/src/Log.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: skplanet
+ * Date: 13. 4. 22.
+ * Time: 오후 5:25
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Log {
+    void write(String data);
+}
